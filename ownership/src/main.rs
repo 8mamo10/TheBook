51cbc04739020a4a63488fd6403cbfe2619c28a2
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-07-25 05:22:44 +0900

    Add reference examples

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -19,13 +19,35 @@ fn main() {
 
     //println!("{}", s); // bad
 
-    let t1 = gives_ownership();
-    let t2 = String::from("hello");
-    let t3 = takes_and_gives_back(t2);
+    let _t1 = gives_ownership();
+    let _t2 = String::from("hello");
+    let _t3 = takes_and_gives_back(_t2);
 
     let a1 = String::from("hello");
     let (a2, len) = calculate_length(a1);
     println!("The length of '{}' is {}.", a2, len);
+
+    let a2 = String::from("hello");
+    let len = calculate_length_ref(&a2);
+    println!("The length of '{}' is {}.", a2, len);
+
+    let mut b = String::from("hello");
+    change(&mut b);
+
+    let mut s = String::from("hello");
+    let _r1 = &mut s;
+    //let r2 = &mut s; // bad
+    //println!("{}, {}", r1, r2);
+
+    let mut s = String::from("hello");
+    let r1 = &s;
+    let r2 = &s;
+    println!("{}, {}", r1, r2);
+    let r3 = &mut s;
+    println!("{}", r3);
+
+    //let reference_to_nothing = dangle(); // bad
+    let _reference_to_something = no_dangle();
 }
 
 fn takes_ownership(some_string: String) {
@@ -48,4 +70,27 @@ fn takes_and_gives_back(a_string: String) -> String {
 fn calculate_length(s: String) -> (String, usize) {
     let length = s.len();
     (s, length)
+}
+
+fn calculate_length_ref(s: &String) -> usize {
+    s.len()
+}
+
+// bad
+// fn change(some_string: &String) {
+//     some_string.push_str(", world!");
+// }
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world!");
+}
+
+// fn dangle() -> &String {
+//     let s = String::from("hello");
+//     &s
+// }
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+    s
 }
