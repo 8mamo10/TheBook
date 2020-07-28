65992f2e5861967b36ffc4edd2b0cf8f74570be6
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-07-29 00:58:57 +0900

    Add struct examples

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,6 +4,21 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        (self.width >= other.width) && (self.height >= other.width)
+    }
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let width1 = 30;
     let height1 = 50;
@@ -28,6 +43,28 @@ fn main() {
     );
     println!("recv2 is {:?}", rect2);
     println!("recv2 is {:#?}", rect2);
+
+    println!(
+        "The area of the rectangle is {} squere pixels.",
+        rect2.area()
+    );
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 40,
+    };
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    Rectangle::square(3);
 }
 
 fn area(width: u32, height: u32) -> u32 {
