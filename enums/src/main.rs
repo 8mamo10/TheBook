64ae85f45e6792600fc3642f101184a1c182cc8e
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-07-31 18:57:44 +0900

    Add enums

---
+++ enums/src/main.rs
@@ -0,0 +1,57 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {}
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
+
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    //let sum = x + y; // bad
+}
+
+fn route(ip_kind: IpAddrKind) {}
