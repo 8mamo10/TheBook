d1cbc0129e08ba1054b515e6c9586a41851b6a78
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-08-02 23:49:49 +0900

    Add if let examples

--- coin/src/main.rs
+++ coin/src/main.rs
@@ -52,4 +52,31 @@ fn main() {
         7 => println!("seven"),
         _ => (),
     }
+    //let some_u8_value = Some(0u8);
+    let some_u8_value = Some(3u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+    let coin: Coin = Coin::Nickel;
+    let mut count = 0;
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+    println!("count is {}", count);
+
+    let coin: Coin = Coin::Quarter(UsState::Alaska);
+    //let coin: Coin = Coin::Penny;
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+    println!("count is {}", count);
 }
