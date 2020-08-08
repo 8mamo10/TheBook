da9db6db90f3384a34e451e3724f686dabedd2c9
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-08-08 18:17:48 +0900

    Add string examples

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -46,4 +46,56 @@ fn main() {
         SpreadsheetCell::Text(String::from("blue")),
         SpreadsheetCell::Float(10.12),
     ];
+
+    let mut s = String::new();
+    println!("new string s={}", s);
+    let data = "initial contents";
+    println!("data={}", s);
+    let s = data.to_string();
+    println!("data.to_string={}", s);
+    let s = "initial contents".to_string();
+    println!("s={}", s);
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    println!("s={}", s);
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2={}", s2);
+
+    let mut s = String::from("lo");
+    s.push('l');
+    println!("s={}", s);
+
+    let s1 = String::from("hello,");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
+    println!("s3={}", s3);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = s1 + "-" + &s2 + "-" + &s3;
+    println!("s={}", s);
+    let s4 = String::from("tic");
+    let s5 = String::from("tac");
+    let s6 = String::from("toe");
+    let s = format!("{}-{}-{}", s4, s5, s6);
+    println!("s={}", s);
+
+    let s1 = String::from("hello");
+    //let h = s1[0]; // bad
+    let hello = "Здравствуйте";
+    let s = &hello[0..4];
+    println!("s={}", s);
+    //let s = &hello[0..1]; // panic
+    //println!("s={}", s);
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
+    for b in "नमस्ते".bytes() {
+        println!("{}", b);
+    }
 }
