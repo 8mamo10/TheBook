a487acc72735710065fdbf224bb7d12a649aae32
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-08-20 23:59:36 +0900

    Test

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let number_list = vec![34, 50, 25, 100, 65, 1000];
     let mut largest = number_list[0];
     for number in number_list {
         if number > largest {
