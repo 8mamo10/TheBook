9b9b1e5c4fddae8c90c81f180816c0bf67a01aff
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-08-21 23:36:23 +0900

    Add largest fn

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,10 +1,37 @@
-fn main() {
-    let number_list = vec![34, 50, 25, 100, 65, 1000];
-    let mut largest = number_list[0];
-    for number in number_list {
-        if number > largest {
-            largest = number;
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+    for &item in list {
+        if item > largest {
+            largest = item;
         }
     }
-    println!("The largest number is {}", largest);
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let mut l = number_list[0];
+    for number in number_list {
+        if number > l {
+            l = number;
+        }
+    }
+    println!("The largest number is {}", l);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let mut l = number_list[0];
+    for number in number_list {
+        if number > l {
+            l = number;
+        }
+    }
+    println!("The largest number is {}", l);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 }
