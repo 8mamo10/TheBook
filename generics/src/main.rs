0e22f2628aafb179311746848bc65f0f3f03f6ae
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-08-23 16:25:23 +0900

    Fix largest_any using trait

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -59,16 +59,15 @@ fn largest_char(list: &[char]) -> char {
     largest
 }
 
-// fn largest_any<T>(list: &[T]) -> T {
-//     let mut largest = list[0];
-//     for &item in list {
-//         // `T` might need a bound for `std::cmp::PartialOrd`
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
-//     largest
-// }
+fn largest_any<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
@@ -105,13 +104,13 @@ fn main() {
     let result = largest_char(&char_list);
     println!("The largest char is {}", result);
 
-    // let number_list = vec![34, 50, 25, 100, 65];
-    // let result = largest_any(&number_list);
-    // println!("The largest number is {}", result);
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest_any(&number_list);
+    println!("The largest number is {}", result);
 
-    // let char_list = vec!['y', 'm', 'a', 'q'];
-    // let result = largest_any(&char_list);
-    // println!("The largest char is {}", result);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_any(&char_list);
+    println!("The largest char is {}", result);
 
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 3.0, y: 4.0 };
