fd13cb3c1903cd6db7f398a504715df065917c08
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-09-17 03:15:23 +0900

    Change the expensive process to closure

--- workout/src/main.rs
+++ workout/src/main.rs
@@ -8,16 +8,19 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = simulated_expensive_calculation(intensity);
-
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_result);
-        println!("Next, do {} situps!", expensive_result);
+        println!("Today, do {} pushups!", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_closure(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break! Remember to stay hydrated!");
         } else {
-            println!("Today, run {} minutes!", expensive_result);
+            println!("Today, run {} minutes!", expensive_closure(intensity));
         }
     }
 }
