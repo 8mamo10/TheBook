81bf2697f44d38c51719d12d1f392431822810ef
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-10-07 00:30:19 +0900

    Add art

---
+++ art/src/main.rs
@@ -0,0 +1,12 @@
+extern crate art;
+
+// use art::kinds::PrimaryColor;
+// use art::utils::mix;
+use art::mix;
+use art::PrimaryColor;
+
+fn main() {
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+    mix(red, yellow);
+}
