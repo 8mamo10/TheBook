350b254cedc2d65480bd64dde4f42f5506ef55cd
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-11-01 23:59:39 +0900

    Use Rc for thread, but it still failed

--- mutex/src/main.rs
+++ mutex/src/main.rs
@@ -1,3 +1,4 @@
+use std::rc::Rc;
 use std::sync::Mutex;
 use std::thread;
 
@@ -9,26 +10,17 @@ fn main() {
     }
     println!("m = {:?}", m);
 
-    let counter = Mutex::new(0);
+    let counter = Rc::new(Mutex::new(0));
     let mut handles = vec![];
 
-    let handle = thread::spawn(move || {
-        let mut num = counter.lock().unwrap();
-    });
-    handles.push(handle);
-
-    let handle2 = thread::spawn(move || {
-        let mut num2 = counter.lock().unwrap();
-    });
-    handles.push(handle2);
-
-    // for _ in 0..10 {
-    //     let handle = thread::spawn(move || {
-    //         let mut num = counter.lock().unwrap();
-    //         *num += 1;
-    //     });
-    //     handles.push(handle);
-    // }
+    for _ in 0..10 {
+        let counter = Rc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
     for handle in handles {
         handle.join().unwrap();
     }
